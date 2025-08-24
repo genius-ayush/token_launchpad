aa0d728586d0072abb9a2963af5ebc7159007560
genius-ayush <ayushsanjayrawal@gmail.com>
2025-08-24 23:55:09 +0530

    setup

--- src/components/TokenLaunchpad.tsx
+++ src/components/TokenLaunchpad.tsx
@@ -1,18 +1,103 @@
-import { createMint } from "@solana/spl-token";
+import { Keypair, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { MINT_SIZE, TOKEN_2022_PROGRAM_ID, createMintToInstruction, createAssociatedTokenAccountInstruction, getMintLen, createInitializeMetadataPointerInstruction, createInitializeMintInstruction, TYPE_SIZE, LENGTH_SIZE, ExtensionType, mintTo, getOrCreateAssociatedTokenAccount, getAssociatedTokenAddressSync } from "@solana/spl-token"
+import { createInitializeInstruction, pack } from '@solana/spl-token-metadata';
+
+
 export function TokenLaunchpad() {
+    const { connection } = useConnection();
+    const wallet = useWallet();
 
-    const createToken = ()=>{
+    const name = document.getElementById("name") as HTMLInputElement;
+    const symbol = document.getElementById("symbol") as HTMLInputElement;
+    const imgURL = document.getElementById("imgURL") as HTMLInputElement;
+    const initialSupply = document.getElementById("initialSupply") as HTMLInputElement;
 
-        const name  = document.getElementById("name") as HTMLInputElement  ; 
-        const symbol = document.getElementById("symbol") as HTMLInputElement  ; 
-        const imgURL = document.getElementById("imgURL") as HTMLInputElement  ; 
-        const initialSupply = document.getElementById("initialSupply") as HTMLInputElement ;
+    async function createToken() {
+        
+        if (!wallet.publicKey) {
+            console.log("no public key found")
+            return
+        }
+        
+        const mintKeypair = Keypair.generate();
 
-        // createMint() ; 
+        console.log(name)
+        console.log("reached here")
+        const metadata = {
+            mint: mintKeypair.publicKey,
+            name: name.value,
+            symbol: symbol.value,
+            uri: imgURL.value,
+            additionalMetadata: [],
+        };
 
-        console.log(name?.value) ; 
+        const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+        const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
+
+        const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
+
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: mintKeypair.publicKey,
+                space: mintLen,
+                lamports,
+                programId: TOKEN_2022_PROGRAM_ID,
+            }),
+            createInitializeMetadataPointerInstruction(mintKeypair.publicKey, wallet.publicKey, mintKeypair.publicKey, TOKEN_2022_PROGRAM_ID),
+
+            createInitializeMintInstruction(mintKeypair.publicKey, 9, wallet.publicKey, null, TOKEN_2022_PROGRAM_ID),
+            createInitializeInstruction({
+                programId: TOKEN_2022_PROGRAM_ID,
+                mint: mintKeypair.publicKey,
+                metadata: mintKeypair.publicKey,
+                name: metadata.name,
+                symbol: metadata.symbol,
+                uri: metadata.uri,
+                mintAuthority: wallet.publicKey,
+                updateAuthority: wallet.publicKey,
+            }),
+        );
+        transaction.feePayer = wallet.publicKey;
+        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+        transaction.partialSign(mintKeypair);
+
+        await wallet.sendTransaction(transaction, connection);
+
+        console.log(`Token mint created at ${mintKeypair.publicKey.toBase58()}`);
+        const associatedToken = getAssociatedTokenAddressSync(
+            mintKeypair.publicKey,
+            wallet.publicKey,
+            false,
+            TOKEN_2022_PROGRAM_ID,
+        );
+
+        console.log(associatedToken.toBase58());
+
+        const transaction2 = new Transaction().add(
+            createAssociatedTokenAccountInstruction(
+                wallet.publicKey,
+                associatedToken,
+                wallet.publicKey,
+                mintKeypair.publicKey,
+                TOKEN_2022_PROGRAM_ID,
+            ),
+        );
+
+        await wallet.sendTransaction(transaction2, connection);
+
+        const transaction3 = new Transaction().add(
+            createMintToInstruction(mintKeypair.publicKey, associatedToken, wallet.publicKey, parseInt(initialSupply.value), [], TOKEN_2022_PROGRAM_ID)
+        );
+
+        await wallet.sendTransaction(transaction3, connection);
+
+        console.log("Minted!")
     }
-    return  <div style={{
+
+    return <div style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , nodePolyfills(),],
+  plugins: [react() , nodePolyfills(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
