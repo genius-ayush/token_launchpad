411a74937563f46c8d6840ef5ca1333a3462baee
genius-ayush <ayushsanjayrawal@gmail.com>
2025-08-27 15:48:35 +0530

    hero image changed again

--- src/components/HeroSectionOne.tsx
+++ src/components/HeroSectionOne.tsx
@@ -94,7 +94,7 @@ export function HeroSectionOne() {
         >
           <div className="w-full overflow-hidden rounded-xl border border-gray-300 dark:border-gray-700">
             <img
-              src="/./public/heroImage.png"
+              src="/public/heroImage.png"
               alt="Landing page preview"
               className="aspect-[16/9] h-auto w-full object-cover"
               height={1000}
